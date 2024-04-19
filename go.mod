9423d7bcf43274a232c76635aefb5472f3c0697f
Elliott Minns <elliott.minns@pm.me>
2024-04-19 09:29:26 -0500

    kafka module test code

--- go.mod
+++ go.mod
@@ -39,7 +39,7 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
-	github.com/klauspost/compress v1.16.0 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
@@ -51,7 +51,7 @@ require (
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0 // indirect
-	github.com/pierrec/lz4/v4 v4.1.16 // indirect
+	github.com/pierrec/lz4/v4 v4.1.18 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
@@ -59,6 +59,7 @@ require (
 	github.com/shirou/gopsutil/v3 v3.23.12 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/testcontainers/testcontainers-go/modules/kafka v0.30.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
--- pubsub/pubsub_test.go
+++ pubsub/pubsub_test.go
@@ -1,1 +1,89 @@
 package pubsub_test
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	"github.com/segmentio/kafka-go"
+	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	kfka "github.com/testcontainers/testcontainers-go/modules/kafka"
+	"github.com/testcontainers/testcontainers-go/wait"
+
+	"github.com/dreamsofcode-io/testcontainers/pubsub"
+)
+
+func TestPubSub(t *testing.T) {
+	ctx := context.Background()
+
+	container, err := kfka.RunContainer(ctx,
+		kfka.WithClusterID("test-cluster"),
+		testcontainers.WithImage("confluentinc/confluent-local:7.5.0"),
+		testcontainers.WithEnv(map[string]string{
+			"KAFKA_CFG_AUTO_CREATE_TOPICS_ENABLE": "true",
+		}),
+		testcontainers.WithWaitStrategy(
+			wait.ForListeningPort("9093/tcp"),
+		),
+	)
+	if err != nil {
+		log.Fatalf("failed to start container: %s", err)
+	}
+
+	endpoint, err := container.PortEndpoint(ctx, "9093/tcp", "")
+	if err != nil {
+		log.Fatalf("failed to get endpoint: %s", err)
+	}
+
+	conn, err := kafka.DialLeader(ctx, "tcp", endpoint, "test-topic", 0)
+	if err != nil {
+		log.Fatalf("failed to dial leader: %s", err)
+	}
+
+	t.Run("single message", func(t *testing.T) {
+		ps := pubsub.New(conn)
+		err = ps.WriteMessage(pubsub.Message{
+			Title:       "Hello, world!",
+			Description: "testcontainers are awesome",
+		})
+
+		assert.NoError(t, err)
+
+		msg, err := ps.ReadMessage(ctx)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "Hello, world!", msg.Title)
+		assert.Equal(t, "testcontainers are awesome", msg.Description)
+	})
+
+	t.Run("multiple messages", func(t *testing.T) {
+		ps := pubsub.New(conn)
+		err = ps.WriteMessage(pubsub.Message{
+			Title:       "Hello, world!",
+			Description: "testcontainers are awesome",
+		})
+
+		assert.NoError(t, err)
+
+		err = ps.WriteMessage(pubsub.Message{
+			Title:       "Another one",
+			Description: "golang is neat too",
+		})
+
+		assert.NoError(t, err)
+
+		msg, err := ps.ReadMessage(ctx)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "Hello, world!", msg.Title)
+		assert.Equal(t, "testcontainers are awesome", msg.Description)
+
+		msg, err = ps.ReadMessage(ctx)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "Another one", msg.Title)
+		assert.Equal(t, "golang is neat too", msg.Description)
+	})
+
+}
